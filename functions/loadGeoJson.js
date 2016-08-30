bbbe321acd92ba43ecab0fa0714952cf381e5877
Renato Mestre <rmestre@sorocaba.sp.gov.br>
2016-08-30 15:52:25 -0300

    Load GeoJson

--- functions/loadGeoJson.js
+++ functions/loadGeoJson.js
@@ -1,24 +1,58 @@
 /*
-	version: 1.0.0
+	version: 1.0.1
 */
-loadGeoJson: function (URL) {
+loadGeoJson: function (args) {
 	var map = this.map,
 		Mapi = this;
 
-	$.getJSON(URL, function (response) {
-		var featureCollection = map.data.addGeoJson(response);
+	args = {
+		URL: args.URL || null,
+		geoJson: args.geoJson || null,
+		collection: args.collection || 'geojsons'
+	};
+
+	if (args.URL) {
+		$.getJSON(URL, function (response) {
+			setGeoJson(response);
+		});
+	}
+
+	if (args.geoJson) {
+		setGeoJson(args.geoJson);
+	}
+
+	function setGeoJson(obj) {
+		if (!obj.type) {
+			throw ('Yout GeoJson must have a Type node');
+		}
+
+		var newObj = {};
+
+		if (obj.type !== 'FeatureCollection') {
+			newObj.type = 'FeatureCollection';
+			newObj.features = [];
+			newObj.features.push(obj);
+		} else {
+			newObj = obj;
+		}
+
+		var featureCollection = map.data.addGeoJson(newObj);
 
 		_.each(featureCollection, function (feature) {
-			// Add each GeoJson feature to the 'regions' collection
-			Mapi.addObject('regions', feature.getId(), feature);
+			// Add each GeoJson feature to defined collection
+			Mapi.addObject({
+				groupId: args.collection,
+				id: feature.getId(),
+				object: feature
+			});
 		});
 
-		// Uses style properties of GeoJson features
+		// Uses some style properties of GeoJson features
 		map.data.setStyle(function (feature) {
 			return ({
 				fillColor: feature.getProperty('fill'),
 				strokeWeight: feature.getProperty('stroke-width')
 			});
 		});
-	});
+	}
 }
